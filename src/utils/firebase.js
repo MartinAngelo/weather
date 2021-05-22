de28703a5f4384f86f2a57a7724dbd194b512db4
MartinAngelo <gelomartin163@gmail.com>
2021-05-22 18:34:33 +0800

    add

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,16 +2,18 @@ import firebase from "firebase";
 
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC2NAyCJlT4LMhTMSCRvQrN1dnGVtXa5-Y",
-    authDomain: "bsit-3c-martinangelo.firebaseapp.com",
-    projectId: "bsit-3c-martinangelo",
-    storageBucket: "bsit-3c-martinangelo.appspot.com",
-    messagingSenderId: "937609379656",
-    appId: "1:937609379656:web:8af8d1c731d52300647d7d",
-    measurementId: "G-EGXZCG2JLD"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+    apiKey: "AIzaSyCG_biTOWAbaBronfDm--2ESBI54tNApW8",
+    authDomain: "weather-6114d.firebaseapp.com",
+    projectId: "weather-6114d",
+    storageBucket: "weather-6114d.appspot.com",
+    messagingSenderId: "747078155804",
+    appId: "1:747078155804:web:1706d81c86ec89ec5369d9",
+    measurementId: "G-EEXGRFV8MS"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
 
 export default firebase;
+
+
